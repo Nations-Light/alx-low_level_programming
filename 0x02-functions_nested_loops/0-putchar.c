69e6829f83109c89b92bb9c19882ab0852522e0c
Nations-Light <queenjoseph661@gmail.com>
2022-12-14 06:59:02 -0800

    Putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - print putchar
+ * Return: always 0
+ */
+
+int main(void)
+{
+	_Putchar('_');
+	_Putchar('p');
+	_Putchar('u');
+	_Putchar('t');
+	_Putchar('c');
+	_Putchar('h');
+	_Putchar('a');
+	_Putchar('r');
+	_Putchar('\n);
+
+	return (0)
+}
