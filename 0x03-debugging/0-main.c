1f6769e12795e60b00c9e2fb569ac28a6baee39a
Nations-Light <queenjoseph661@gmail.com>
2022-12-15 12:50:40 -0800

    main files

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,6 +1,5 @@
 #include "main.h"
 
-
 /**
 
 * main - Test function for positive or negative
@@ -9,21 +8,16 @@
 
 */
 
-
 int main(void)
 
 {
 
 int i;
 
-
 i = 0;
 
 positive_or_negative(i);
 
-
 return (0);
 
 }
-
-
